bf13c6096503060a3bba99404e052f4c1738f85b
Josa Gesell <josa@gesell.me>
2021-12-11 13:18:01 +0100

    feat: Extract functionality from josa42/config-nvim

--- lua/jg/lsp-codelens.lua
+++ lua/jg/lsp-codelens.lua
@@ -1,7 +1,35 @@
 local M = {}
+local l = {}
 
 function M.setup()
-  print("setup: nvim-lsp-codelens")
+  vim.cmd([[
+    hi! link LspCodeLens          Comment
+    hi! link LspCodeLensSeparator Comment
+  ]])
+
+  vim.cmd([[
+    augroup lsp-codelens
+      au!
+      au! BufEnter,CursorHold,InsertLeave <buffer> lua require('jg.lsp-codelens').on_refresh()
+    augroup END
+  ]])
+end
+
+function M.on_refresh()
+  if l.anyClientSupports('textDocument/codeLens') then
+    vim.lsp.codelens.refresh()
+  end
+end
+
+function l.anyClientSupports(method)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local supported = false
+  vim.lsp.for_each_buffer_client(bufnr, function(client)
+    if client.supports_method(method) then
+      supported = true
+    end
+  end)
+  return supported
 end
 
 return M
